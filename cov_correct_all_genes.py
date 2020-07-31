49750f697a5bd1d4d47bfd3e14132f8c1ecf2af3
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2020-07-31 12:43:17 -0500

    Add files via upload

---
+++ cov_correct_all_genes.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+input_directory = sys.argv[1]
+
+#create list of kegg files to parse through
+protein_files = [file for file in os.listdir(input_directory) if file.endswith(".faa")]
+
+#Sum coverage of every gene per bacterial prodigal file
+for annot in protein_files:
+	with open(os.path.join(input_directory, annot)) as input:
+		count = 0
+		for line in input:
+			if line.startswith(">NODE"):
+				cov = float(line.split("_")[5])
+				count = count + cov
+			else:
+				pass
+	print(str(annot)+" summed gene count (cov corrected) is: "+str(count))
+	input.close()
